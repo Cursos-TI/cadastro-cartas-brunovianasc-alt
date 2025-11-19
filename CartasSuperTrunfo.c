354d9fd0be75943535fc177224dcdd4b2f16274c
brunovianasc-alt <brunoviana.sc@gmail.com>
2025-11-19 14:55:46 +0000

    Criação e execução do registro de cartas do jogo Super Trunfo.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -76,7 +76,7 @@ int main() {
     printf("=============================\n");
     printf("Estado: %c\n", estado1);
     printf("Codigo: %s\n", codigo1);
-    printf("Nome da Cidade: %s\n", cidade1);
+    printf("Cidade: %s\n", cidade1);
     printf("Populacao: %d\n", populacao1);
     printf("Area: %.2f km²\n", area1);
     printf("PIB: %.2f bilhoes de reais\n", pib1);
@@ -87,7 +87,7 @@ int main() {
     printf("=============================\n");
     printf("Estado: %c\n", estado2);
     printf("Codigo: %s\n", codigo2);
-    printf("Nome da Cidade: %s\n", cidade2);
+    printf("Cidade: %s\n", cidade2);
     printf("Populacao: %d\n", populacao2);
     printf("Area: %.2f km²\n", area2);
     printf("PIB: %.2f bilhoes de reais\n", pib2);
